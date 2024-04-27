714968017e9d562727720f5300f404ca2559e767
amullikin <avalynmullikin@gmail.com>
2024-04-27 01:55:57 -0400

    Added some add and remove procedures

--- server.js
+++ server.js
@@ -42,6 +42,16 @@ app.get('/customers', (req, res) => {
     res.sendFile(__dirname + '/customers.html');
 });
 
+app.get('/drones', (req, res) => {
+
+    res.sendFile(__dirname + '/drones.html');
+});
+
+app.get('/products', (req, res) => {
+
+    res.sendFile(__dirname + '/products.html');
+});
+
 app.get('/views', (req, res) => {
 
     res.sendFile(__dirname + '/views.html');
@@ -50,7 +60,7 @@ app.get('/views', (req, res) => {
 app.post('/add_customer', (req, res) => {
     const data = req.body;
     
-    console.log(data)   // Assuming the request body contains 'name' and 'email' fields
+    console.log(data)
 
   
     // Perform the MySQL query to insert the new customer
@@ -71,7 +81,116 @@ app.post('/add_customer', (req, res) => {
 
   });
 
+  // Add drone pilot
+  app.post('/add_drone_pilot', (req, res) => {
+    const data = req.body;
+    
+    console.log(data)
 
+  
+    // Insert new drone pilot
+    connection.query('call add_drone_pilot (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)',
+     [data.username, data.first_name, data.last_name, data.address, data.birthdate, data.taxID, data.service, data.salary, data.licenseID, data.experience], (error, results, fields) => {
+      if (error) {
+        console.error('Error adding pilot: ' + error);
+        res.status(500).json({ error: 'Error adding pilot' });
+        return;
+      }
+      
+    });
+
+
+    res.sendFile(__dirname + '/drones.html');
+
+  });
+
+  app.post('/add_product', (req, res) => {
+    const data = req.body;
+    
+    console.log(data)
+
+  
+    // Insert new product
+    connection.query('call add_product (?, ?, ?)',
+     [data.barcode, data.name, data.weight], (error, results, fields) => {
+      if (error) {
+        console.error('Error adding product: ' + error);
+        res.status(500).json({ error: 'Error adding product' });
+        return;
+      }
+      
+    });
+
+
+    res.sendFile(__dirname + '/products.html');
+
+  });
+
+  app.post('/remove_customer', (req, res) => {
+    const data = req.body;
+    
+    console.log(data)
+
+  
+    // Remove customer
+    connection.query('call remove_customer (?)',
+     [data.username], (error, results, fields) => {
+      if (error) {
+        console.error('Error removing customer: ' + error);
+        res.status(500).json({ error: 'Error removing customer' });
+        return;
+      }
+      
+    });
+
+
+    res.sendFile(__dirname + '/customers.html');
+
+  });
+
+  app.post('/remove_drone_pilot', (req, res) => {
+    const data = req.body;
+    
+    console.log(data)
+
+  
+    // Remove drone pilot
+    connection.query('call remove_drone_pilot (?)',
+     [data.username], (error, results, fields) => {
+      if (error) {
+        console.error('Error removing drone pilot: ' + error);
+        res.status(500).json({ error: 'Error removing drone pilot' });
+        return;
+      }
+      
+    });
+
+
+    res.sendFile(__dirname + '/drones.html');
+
+  });
+
+  app.post('/remove_product', (req, res) => {
+    const data = req.body;
+    
+    console.log(data)
+
+  
+    // Remove product
+    connection.query('call remove_product (?)',
+     [data.barcode], (error, results, fields) => {
+      if (error) {
+        console.error('Error removing product: ' + error);
+        res.status(500).json({ error: 'Error removing product' });
+        return;
+      }
+      
+    });
+
+
+    res.sendFile(__dirname + '/products.html');
+
+  });
 
   app.get('/customer_credit_check', (req, res) => {
     // Query the database to get customer credit check data
@@ -90,7 +209,7 @@ app.post('/add_customer', (req, res) => {
 });
 
 app.get('/drone_pilot_roster', (req, res) => {
-    // Query the database to get customer credit check data
+    // Query the database to get drone_pilot_roster data
     connection.query('SELECT * FROM drone_pilot_roster', (error, results, fields) => {
         if (error) {
             console.error('Error querying view: ' + error);
@@ -105,8 +224,85 @@ app.get('/drone_pilot_roster', (req, res) => {
     });
 });
 
+app.get('/drone_traffic_control', (req, res) => {
+    // Query the database to get drone_traffic_control data
+    connection.query('SELECT * FROM drone_traffic_control', (error, results, fields) => {
+        if (error) {
+            console.error('Error querying view: ' + error);
+            res.status(500).send('Error querying view');
+            return;
+        }
+        console.log('Results from view:');
+        console.log(results);
 
+        // Render the EJS template with the retrieved data
+        res.render('drone_traffic_control', { data: results });
+    });
+});
 
+app.get('/most_popular_products', (req, res) => {
+    // Query the database to get most_popular_product data
+    connection.query('SELECT * FROM most_popular_products', (error, results, fields) => {
+        if (error) {
+            console.error('Error querying view: ' + error);
+            res.status(500).send('Error querying view');
+            return;
+        }
+        console.log('Results from view:');
+        console.log(results);
+
+        // Render the EJS template with the retrieved data
+        res.render('most_popular_products', { data: results });
+    });
+});
+
+app.get('/orders_in_progress', (req, res) => {
+    // Query the database to get orders_in_progress data
+    connection.query('SELECT * FROM orders_in_progress', (error, results, fields) => {
+        if (error) {
+            console.error('Error querying view: ' + error);
+            res.status(500).send('Error querying view');
+            return;
+        }
+        console.log('Results from view:');
+        console.log(results);
+
+        // Render the EJS template with the retrieved data
+        res.render('orders_in_progress', { data: results });
+    });
+});
+
+app.get('/role_distribution', (req, res) => {
+    // Query the database to get role_distribution data
+    connection.query('SELECT * FROM role_distribution', (error, results, fields) => {
+        if (error) {
+            console.error('Error querying view: ' + error);
+            res.status(500).send('Error querying view');
+            return;
+        }
+        console.log('Results from view:');
+        console.log(results);
+
+        // Render the EJS template with the retrieved data
+        res.render('role_distribution', { data: results });
+    });
+});
+
+app.get('/store_sales_overview', (req, res) => {
+    // Query the database to get store_sales_overview data
+    connection.query('SELECT * FROM store_sales_overview', (error, results, fields) => {
+        if (error) {
+            console.error('Error querying view: ' + error);
+            res.status(500).send('Error querying view');
+            return;
+        }
+        console.log('Results from view:');
+        console.log(results);
+
+        // Render the EJS template with the retrieved data
+        res.render('store_sales_overview', { data: results });
+    });
+});
 
 // Define the port number
 const port = process.env.PORT || 3000;
