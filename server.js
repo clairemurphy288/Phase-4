6e9bcce50ad1ca3ccf3ced1f80f6d15f7bcb5c45
Claire Murphy <murphyclaire91@gmail.com>
2024-04-28 18:19:53 -0400

    removing random print statement

--- server.js
+++ server.js
@@ -21,15 +21,6 @@ var connection = mysql.createConnection({
 
 connection.connect();
 
-connection.query('SELECT 1 + 1 AS solution', function (error, results, fields) {
-    if (error) throw error;
-    console.log('The solution is: ', results[0].solution);
-});
-
-
-
-
-
 
 // Define a route handler for the root path
 app.get('/', (req, res) => {
